b428a31cff880390f222f89c050286769926e25d
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2026-04-08 12:56:18 +1200

    feat(main menu): create basic main menu layout

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,12 @@
+#handles the main menu inputs
+
+extends Control
+
+var gameScene = preload("res://Scenes/MainGame.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
