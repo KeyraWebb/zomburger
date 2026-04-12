15470d3c7e74c0e0093e2418c49fb0edc3b8f0ee
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2026-04-13 10:29:35 +1200

    feat(player): add basic player movement

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,25 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _physics_process(_delta: float) -> void:
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	get_input()
 	move_and_slide()
+
+#I stole this from the godot tuts: https://docs.godotengine.org/en/stable/tutorials/2d/2d_movement.html
+func get_input(): 
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = input_direction * speed
